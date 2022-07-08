609b8bf7f5b9badc25a64cfd87686b9b7c032d41
Tobias Windisch <tobias.windisch@posteo.de>
2022-07-08 20:35:38 +0200

    add node groups and sample edges within

--- bn_testing/models.py
+++ bn_testing/models.py
@@ -8,8 +8,10 @@ class Model(object):
 
 
 class ConditionalGaussian(Model):
+    """
+    """
 
-    def __init__(self, parents, random_state=10):
+    def __init__(self, parents, random_state=10, sigma=0.1):
 
         self.parents = parents
 
@@ -20,15 +22,17 @@ class ConditionalGaussian(Model):
             high=1,
             size=len(self.parents)
         )
-        self.sigma = 0.1
+        self.sigma = sigma
+
+    def _get_mean(self, X):
+        return np.matmul(X, self.weights)
 
     def sample(self, df):
-
+        """
+        Samples for each row of the :py:cls:`pandas.DataFrame` an element from the conditional
+        distribution.
+        """
         X = df[self.parents].values
-
-        # TODO: Allow more transformations
-        means = np.matmul(X, self.weights)
-
         return self.random.normal(
-            loc=means,
+            loc=self._get_mean(X),
             scale=self.sigma)
--- tests/test_dags.py
+++ tests/test_dags.py
@@ -1,5 +1,6 @@
 import unittest
 import networkx as nx
+import numpy as np
 
 from bn_testing.graphs import GraphicalModel
 
@@ -7,10 +8,22 @@ from bn_testing.graphs import GraphicalModel
 class TestRandomDAG(unittest.TestCase):
 
     def setUp(self):
-        self.bn = GraphicalModel(n_nodes=10, p=0.1)
+        self.bn = GraphicalModel(n_nodes=10, n_groups=2, p=0.1)
 
     def test_acyclicity(self):
         self.assertTrue(nx.is_directed_acyclic_graph(self.bn.dag))
+
+    def test_group_names(self):
+        assert False
+
+    def test_nodes(self):
+        np.testing.assert_array_equal(
+            self.bn.nodes,
+            [
+                'g0_f0', 'g0_f1', 'g0_f2', 'g0_f3', 'g0_f4',
+                'g1_f0', 'g1_f1', 'g1_f2', 'g1_f3', 'g1_f4',
+            ]
+        )
 
     def test_models(self):
         self.assertIsInstance(self.bn.models, dict)
